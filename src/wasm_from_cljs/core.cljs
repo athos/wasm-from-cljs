1890142b43d692fc662ea59ca969dd0d75d125eb
Shogo Ohta <athos0220@gmail.com>
2018-10-31 00:34:30 +0900

    Add simple wasm loading code

--- src/wasm_from_cljs/core.cljs
+++ src/wasm_from_cljs/core.cljs
@@ -1,6 +1,31 @@
-(ns wasm-from-cljs.core)
+(ns wasm-from-cljs.core
+  (:require [aintegrant.core :as ag]
+            [kitchen-async.promise :as p]
+            [integrant.core :as ig]))
+
+(enable-console-print!)
+
+(defmethod ag/init-key :wasm [_ {:keys [path]} callback]
+  (p/let [resp (js/fetch path)
+          bytes (.arrayBuffer resp)
+          result (js/WebAssembly.instantiate bytes #js {})]
+    (callback (.. result -instance -exports))))
+
+(defmethod ig/init-key :facts [_ {:keys [wasm]}]
+  (mapv #(.fact wasm %) (range 10)))
+
+(def config
+  {:wasm {:path "/factorial.wasm"}
+   :facts {:wasm (ig/ref :wasm)}})
+
+(def system (atom nil))
 
 (defn -main []
-  (js/alert "Hello, World!"))
+  (p/try
+    (p/let [x ((p/promisify ag/init) config)]
+      (js/alert (str "facts: " (:facts x)))
+      (reset! system x))
+    (p/catch :default e
+      (js/console.log e))))
 
-(-main)
+(.addEventListener js/window "load" -main)
